7960809d93070bd4333d5747ef343348e018ed33
rhy221 <ghuy9366@gmail.com>
2025-10-03 14:11:29 +0700

    Initial commit

---
+++ Assets/Scripts/Firebase/FirebaseAuthService.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+public class FirebaseAuthServie
+{
+    private string apiKey;
+    private string IdToken { get; set; }
+
+    public IEnumerable Login()
+    {
+        
+    }
+}
