d5b3826428ebb558cdb90cd5e995ca113eb0d1a6
Hoang-hai-yen <23521847@gm.uit.edu.vn>
2025-10-09 14:16:31 +0700

    Add logic stamina

--- Assets/Scripts/ClockUI.cs
+++ Assets/Scripts/ClockUI.cs
@@ -11,7 +11,7 @@ public class ClockUI : MonoBehaviour
         clockText = GetComponent<TextMeshProUGUI>();
         if (timeController == null)
         {
-            timeController = FindObjectOfType<TimeController>();
+            timeController = FindFirstObjectByType<TimeController>();
         }
     }
 
